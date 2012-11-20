a6d5c5e8f22c88cf8b2f2f424e3bb65fb3118a9c
chaocui <cc.chaocui@gmail.com>
2012-11-20 16:40:53 -0500

    Test listen to request come in

---
+++ ccsocket.h
@@ -0,0 +1,30 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+struct sockaddr_in name;
+
+int init_listen(int all, in_addr_t addr, int port);
+
+int create_connection();
+
+int init_listen(int all, in_addr_t addr, int port){
+	int sockfd;
+	if((sockfd = socket(AF_INET,SOCK_DGRAM,0)) < 0){
+		fprintf(stderr,"generate socket error\n");
+		exit(1);
+	}
+	
+	name.sin_family = AF_INET;
+	name.sin_port = port;
+	if(all == 1)
+		name.sin_addr.s_addr = INADDR_ANY;
+	if(all == 0)
+		name.sin_addr.s_addr = addr;
+	
+	if(bind(sockfd,(struct sockaddr *)&name, sizeof(name))){
+		fprintf(stderr,"bind error");
+		exit(1);
+	}
+	return sockfd;
+}
--- sws.c
+++ sws.c
@@ -1,2 +1,75 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "ccsocket.h"
+
+int c_flag = 0;
+int d_flag = 0;
+int h_flag = 0;
+int i_flag = 0;
+int l_flag = 0;
+int p_flag = 0;
+char *dir, *CGI_dir, *log_file, *addr;
+int port = 8080;
+
+void usage();
+
+int main(int argc, char **argv){
+	char opt;
+	while((opt = getopt(argc,argv,"c:dhi:l:p:")) != -1){
+		switch(opt){
+			case 'c':
+				c_flag = 1;
+				CGI_dir = optarg;
+				break;
+			case 'd':
+				d_flag = 1;
+				break;
+			case 'h':
+				h_flag = 1;
+				break;
+			case 'i':
+				i_flag = 1;
+				addr = optarg;
+				break;
+			case 'l':
+				l_flag = 1;
+				log_file = optarg;
+				break;
+			case 'p':
+				p_flag = 1;
+				port = atoi(optarg);
+				break;
+		}
+	}	
+	dir = argv[optind];	
+	if(dir == NULL){
+		fprintf(stderr,"Missing dir arguments. Fail to start daemon process.\nUsage:\n");
+		usage();
+		exit(1);
+	}
+	int sockfd;
+	if((sockfd = init_listen(1,NULL,port)) < 0){
+		fprintf(stderr,"cannot start server\n");
+		exit(1);
+	}
+
+	char buf[1024];
+
+	if(read(sockfd,buf,1024) < 0){
+		fprintf("stderr", "Reading from socket error");
+		exit(1);
+	}
+	
+	printf("%s",buf);	
+
+	return 0;
+}
+
+void usage(){
+	printf("sws [options:-c:dhi:l:p:] dir\n");
+}
